b05a5c84a0101e7f687d0831ec019402cfa047b6
Luis Masuelli <luis@datagran.io>
2023-05-21 15:45:32 -0500

    Adding the support for a themed background image

--- Scripts/ThemedButtons.gd
+++ Scripts/ThemedButtons.gd
@@ -8,6 +8,9 @@ func set_button_colors_and_update(theme):
 	for child in get_tree().get_nodes_in_group("ThemedAxes"):
 		child.set_meta("color", theme)
 		child.update_theme()
+	for child in get_tree().get_nodes_in_group("ThemedImages"):
+		child.set_meta("color", theme)
+		child.update_theme()
 
 
 func _ready():
